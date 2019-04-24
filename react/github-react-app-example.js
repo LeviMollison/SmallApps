0dd3320ed5298e3100a24d39cbaacc72ef9c08e5
Levi Mollison <mollisonlevi@gmail..com>
2019-04-23 22:22:15 -0400

    Progress Update
    Uodating code learned from React course: keeping up with github app example

--- react/github-react-app-example.js
+++ react/github-react-app-example.js
@@ -1,44 +1,74 @@
-
-	const testData = [
-			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  		{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-	];
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
 
 
 const CardList = (props) => (
   <div> 
-    {testData.map(testDataMapElement => <Card {...testDataMapElement} />)}
+    <Form />
+    {props.profiles.map(profile => <Card {...profile} />)}
   </div>
 );
 
 class Card extends React.Component {
-	render() {
+  render() {
     const profile = this.props;
-  	return (
-    	<div className="github-profile">
-    	  <img src={profile.avatar_url} />
+    return (
+      <div className="github-profile">
+        <img src={profile.avatar_url} />
         <div className="info">
           <div className="name">{profile.name}</div>
           <div className="company">{profile.company}</div>
         </div>
-    	</div>
+      </div>
     );
   }
 }
 
+class Form extends React.Component {
+  // userNameInput = React.createRef();
+  state = {userName: ''}
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(
+      //this.userNameInput.current.value
+    )
+  };
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit} action="">
+        <input 
+          type="text" 
+          placeholder="Github username" 
+          // ref={this.userNameInput} 
+          required
+        />
+        <button>Add Card</button>
+      </form>
+  )}
+}
+
 class App extends React.Component {
-	render() {
-  	return (
-    	<div>
-    	  <div className="header">{this.props.title}</div>
-        <CardList />
-    	</div>
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     profiles: testData
+  //   };
+  // }
+  state = {profiles: testData};
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <CardList profiles={this.state.profiles}/>
+      </div>
     );
-  }	
+  } 
 }
 
 ReactDOM.render(
-	<App title="The GitHub Cards App" />,
+  <App title="The GitHub Cards App" />,
   mountNode,
 );
