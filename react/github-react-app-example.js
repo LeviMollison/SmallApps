e5b8d2ea20592fa78e2cc01f6b3ea1bf81e4fb8b
levimollison <mollisonlevi@gmail.com>
2019-04-24 01:12:42 -0400

    Finishing up Example
    Finished up github card apps example. Errors have not been handled. Need to try challenge: convert class components into function components. Also need to add error handling

--- react/github-react-app-example.js
+++ react/github-react-app-example.js
@@ -1,14 +1,20 @@
-const testData = [
-    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-];
+// const testData = [
+//     {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+//     {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+//     {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+// ];
 
+// What didn't we do: handle errors
+/*
+  What if they input invalid handle?
+  What should UI do if request fails over the network?
+  What should UI do if request taking too long?
+*/
 
 const CardList = (props) => (
+  // passing key gives react the value to use as the primary-key for the data
   <div> 
-    <Form />
-    {props.profiles.map(profile => <Card {...profile} />)}
+    {props.profiles.map(profile => <Card key={profile.id} {...profile} />) }
   </div>
 );
 
@@ -27,15 +33,30 @@ class Card extends React.Component {
   }
 }
 
+// Form is doing too much: getting the data, then handling it. Component shouldnt have
+// this much responibilty
+// Entire app shouldnt rely directly on axios library, should have small agent type module that has
+// one responsibility to communicate with external API, then have code rely on that instead
+
+/*
+  Exercise: convert class components into function components
+*/
 class Form extends React.Component {
   // userNameInput = React.createRef();
+  // Fetch is good, but there's also axios
   state = {userName: ''}
-  handleSubmit = (event) => {
+  // need async keyword to use await
+  handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(
-      //this.userNameInput.current.value
-      this.state.userName
-    )
+    // returns a promise
+    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+    // console.log(
+    //   //this.userNameInput.current.value
+    //   //this.state.userName
+    //   resp.data // axios returns data object with json data parsed for us
+    // )
+    this.props.onSubmit(resp.data);
+    this.setState({userName: ''}) //Resets username getting tracked by react
   };
   render(){
     return (
@@ -61,11 +82,19 @@ class App extends React.Component {
   //     profiles: testData
   //   };
   // }
-  state = {profiles: testData};
+  state = {profiles: []};
+  // use this to allow children to change parent's state
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      // spread operator syntax for contatenating data
+      profiles: [...prevState.profiles, profileData]
+    })) // need this func to change state of a react component
+  };
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile} />
         <CardList profiles={this.state.profiles}/>
       </div>
     );
